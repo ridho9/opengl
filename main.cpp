ba7eab04a24f6d4ee6373bba79ec05766f324af4
Ridho Pratama <p.ridho@yahoo.co.id>
2019-05-01 17:04:22 +0700

    Implemented 3d camera 1

--- main.cpp
+++ main.cpp
@@ -16,8 +16,18 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+float lastX = WINDOW_WIDTH / 2, lastY = WINDOW_HEIGHT / 2;
+float yaw = -90, pitch = 0;
+
 // Function to listen and handle keypress
 void processInput(GLFWwindow *window);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+
+float zoom = 45.0f;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 5.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 int main(void)
 {
@@ -39,6 +49,9 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
 
     if (!gladLoadGL())
     // if (glewInit() != GLEW_OK)
@@ -105,17 +118,7 @@ int main(void)
 
     // model, set object scale pos rot here
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     shader.setMat4("model", model);
-
-    // view, set camera pos here
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    shader.setMat4("view", view);
-
-    // projection, set camera projection here
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1.0f * WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
-    shader.setMat4("projection", projection);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -125,6 +128,15 @@ int main(void)
         // Background color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // view, set camera pos here
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        shader.setMat4("view", view);
+
+        // projection, set camera projection here
+        glm::mat4 projection = glm::perspective(glm::radians(zoom), 1.0f * WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
+        shader.setMat4("projection", projection);
 
         // bind texture
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -151,4 +163,50 @@ void processInput(GLFWwindow *window)
     // Escape key for closing
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, 1);
+
+    float cameraSpeed = 0.05f; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
+{
+    if (zoom >= 1.0f && zoom <= 45.0f)
+        zoom -= yoffset;
+    if (zoom <= 1.0f)
+        zoom = 1.0f;
+    if (zoom >= 45.0f)
+        zoom = 45.0f;
+}
+
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
 }
